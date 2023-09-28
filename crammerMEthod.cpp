7a7b4e220934f67bf36f4aab6280097ceb5aa83e
sajak <sajakbasnet77@gmail.com>
2023-09-28 12:51:11 +0545

    added crammer rule solver

--- crammerMEthod.cpp
+++ crammerMEthod.cpp
@@ -49,11 +49,12 @@ int determinant3x3(int matrix[3][3]){
     return det;
 }
 
-void initialize(int matrix [3][3]){
+//mainInitializes main matrix from equation
+void mainInitialize(int matrix [3][3]){
     for (int i = 0; i < 3; i++)
     {
         for(int j = 0 ; j < 3; j++){
-            cout <<"Enter value at "<<i <<j <<": ";
+            cout <<"Enter value at "<<i <<j <<" : ";
             cin >> matrix[i][j];
         }
     }
@@ -69,25 +70,97 @@ void constantarray(int constant[3]){
     
 }
 
-void printMatrix(int array[3]) {
+
+void includeconstantInFirst(int matrix[3][3], int cMAtrix[3]) {
+    int j = 0;
     for (int i = 0; i < 3; i++)
     {
-        cout << array[i] <<endl;
+        matrix [i][j] = cMAtrix[i];
+    }
+    
+}
+
+void includeconstantInSecond(int matrix[3][3], int cMAtrix[3]) {
+    int j = 1;
+    for (int i = 0; i < 3; i++)
+    {
+        matrix [i][j] = cMAtrix[i];
+    }
+    
+}
+
+void includeconstantInThird(int matrix[3][3], int cMAtrix[3]) {
+    int j = 2;
+    for (int i = 0; i < 3; i++)
+    {
+        matrix [i][j] = cMAtrix[i];
     }
     
 }
 
 
+void copyMatrix(int source[3][3], int dest[3][3]) {
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            source[i][j] = dest[i][j];
+        }
+        
+    }
+    
+}
+
+void displayMatrix(int matrix [3][3]) {
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+}
+
 
 int main() {
+    int originalMatrix[3][3];
     int mainMatrix[3][3];
+    int firstMatrix[3][3];
+    int secondMatrix[3][3];
+    int thirdMatrix[3][3];
+
     int constantValues[3];
 
-    initialize(mainMatrix);
-    constantarray(constantValues);
+    cout <<"Initializing the main matrix: " <<endl;
+    mainInitialize(mainMatrix);
 
-    cout <<"The determinant of matrix is: "<<determinant3x3(mainMatrix);
+    copyMatrix(originalMatrix,mainMatrix);
+    copyMatrix(firstMatrix,mainMatrix);
+    copyMatrix(secondMatrix,mainMatrix);
+    copyMatrix(thirdMatrix,mainMatrix);
+
+    displayMatrix(firstMatrix);
+
+    // constantarray(constantValues);
     
+    // includeconstantInFirst(mainMatrix,constantValues);
+    // // displayMatrix(mainMatrix);
+    // copyMatrix(firstMatrix,mainMatrix);
+    // cout <<"The determinant of first matrix is " <<determinant3x3(firstMatrix) <<endl;
+
+    // includeconstantInSecond(mainMatrix,constantValues);
+    // // displayMatrix(mainMatrix);
+    // copyMatrix(secondMatrix,mainMatrix);
+    // cout <<"The determinant of second matrix is " <<determinant3x3(secondMatrix) <<endl;
+
+    // includeconstantInThird(mainMatrix,constantValues);
+    // copyMatrix(thirdMatrix,mainMatrix);
+    // cout <<"The determinant of third matrix is " <<determinant3x3(thirdMatrix) <<endl;
+    
+
+
+    cout <<"completed";
 
     return 0;
 }

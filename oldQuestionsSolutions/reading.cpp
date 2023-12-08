bc889284ab6aa41148f0e5d7a37298817cd18769
sajak <sajakbasnet77@gmail.com>
2023-12-08 17:20:08 +0545

    added more programs

---
+++ oldQuestionsSolutions/reading.cpp
@@ -0,0 +1,41 @@
+//write a c++ program to write "PURBANCHAL UNIVERSITY" on a file and read it back to display in console.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    // Writing to a file
+    std::ofstream outFile("university.txt");
+
+    if (!outFile) {
+        std::cerr << "Error opening file for writing." << std::endl;
+        return 1;
+    }
+
+    std::string universityName = "PURBANCHAL UNIVERSITY";
+    outFile << universityName;
+
+    // Close the file
+    outFile.close();
+
+    // Reading from the file
+    std::ifstream inFile("university.txt");
+
+    if (!inFile) {
+        std::cerr << "Error opening file for reading." << std::endl;
+        return 1;
+    }
+
+    // Read the entire line from the file
+    std::string readText;
+    std::getline(inFile, readText);
+
+    // Close the file
+    inFile.close();
+
+    // Display the content in the console
+    std::cout << "Content read from the file: " << readText << std::endl;
+
+    return 0;
+}

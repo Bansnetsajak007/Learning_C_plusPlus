6edd898dc4f1a1c9992004b37a0f3a1bb43854c7
sajak <sajakbasnet77@gmail.com>
2023-12-23 08:29:37 +0545

    added linked list program

---
+++ oldQuestionsSolutions/linkedList.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+
+using namespace std;
+
+// model to structure linked list node
+ struct Node {
+    int data; //only integer type
+    Node* next;
+    Node(int value) : data(value) , next(nullptr) {}
+};
+
+class LinkedList {
+    private:
+        Node* head; //creating first element of linked list
+
+    public:
+        LinkedList() : head(nullptr) {}
+
+        void insertAtBeginning(int value) {
+            Node* newNode = new Node(value); //init the value
+            newNode->next = head;
+            head = newNode;  //making new elemeant head
+        }
+
+        void deleteNode(int value) {
+            if(head == nullptr) {
+                cout << "List is empty. Cannot Delete\n";
+                return; //terminats the program
+            }
+
+            Node* current = head;
+            Node* prev = nullptr;
+
+            while(current != nullptr && current->data !=value) {
+                /*
+                as soon as the condiition evaluates as false
+                loop terminates
+                */
+                prev = current;
+                current = current->next;
+            }
+
+            if(current == nullptr) {
+                cout<< "Elemtnt with" <<value <<"notfound\n";
+                return;
+            }
+
+            prev->next = current->next;
+            delete current;
+        }
+
+        void displayList() {
+            Node* current = head;
+            while(current != nullptr) {
+                cout<<current->data <<" ";
+                current= current->next;
+            }
+        }
+};
+
+
+int main() {
+    LinkedList mylist;
+
+    mylist.insertAtBeginning(7);
+    mylist.insertAtBeginning(9);
+    mylist.insertAtBeginning(6);
+    mylist.insertAtBeginning(5);
+
+    mylist.insertAtBeginning(2);
+    mylist.deleteNode(9);
+    mylist.displayList();
+    return 0;
+}
+
+
+
--- oldQuestionsSolutions/tempCodeRunnerFile.cpp
+++ oldQuestionsSolutions/tempCodeRunnerFile.cpp
@@ -1,19 +1,4 @@
-#include <fstream>
-#include <iostream>
-
-using namespace std;
-int main() {
-
-ifstream file;
-file.open("university.txt", ios::in);
-if (!file.is_open()) {
-cerr << "Failed to open file" << endl;
-return 1;
-}
-string line;
-while (getline(file , line)) {
-cout << line << endl;
-}
-file.close();
-return 0;
-}
+    // mylist.insertAtBeginning(7);
+    // mylist.insertAtBeginning(9);
+    // mylist.insertAtBeginning(6);
+    // mylist.insertAtBeginning(5);

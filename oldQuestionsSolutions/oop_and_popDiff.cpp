d20a3978571cc66622e462f13fc4440d728cd93c
sajak <sajakbasnet77@gmail.com>
2023-12-25 21:04:30 +0545

    added OOP_POPdiff

---
+++ oldQuestionsSolutions/oop_and_popDiff.cpp
@@ -0,0 +1,27 @@
+/*
+
+OOP:
+
+Think objects: Programs are built around objects, which are self-contained entities that combine data (properties) and functionality (methods). Imagine objects like real-world things like cars, with properties like color and wheels, and methods like start() and stop().
+Data hiding: Data is protected within objects and can only be accessed through their methods. This prevents unauthorized access and promotes data integrity. Think of it like a car's engine having internal parts not everyone can touch, only trusted methods like "changeOil()" can access them.
+Inheritance: Objects can inherit properties and methods from parent objects, building upon existing functionality. Imagine a "Truck" inheriting from a "Car," automatically getting properties like wheels and methods like start(), while adding specific truck features like "haulCargo()".
+Polymorphism: Objects of different types can respond to the same message in different ways. Imagine sending a "drive()" message to both a car and a truck. Each will drive, but how they drive (speed, sound) might differ based on their specific types.
+POP:
+
+Think functions: Programs are built around functions that perform specific tasks, typically manipulating data stored in separate variables. Imagine each task being a separate recipe with steps, but the ingredients (data) are kept in different cupboards.
+Global data: Data is often freely accessible throughout the program, potentially leading to inconsistencies and errors. Think of everyone having access to all the ingredients in the kitchen, which can get messy and lead to accidental changes.
+No direct inheritance: Functionality is copied or rewritten instead of inherited, leading to code duplication and maintenance challenges. Imagine building separate recipes for "Truck driving" and "Car driving" instead of sharing common steps through inheritance.
+Limited flexibility: Handling different types of data or responding to messages can be more complex and require specific conditional checks. Imagine having to write different "drive()" functions for every vehicle type instead of using polymorphism.
+In short:
+
+OOP: Organizes programs around independent, reusable objects like real-world entities.
+POP: Organizes programs around functions that operate on separate data, like specific tasks with individual tools.
+Choosing between OOP and POP:
+
+OOP: Preferred for complex projects with reusable features and diverse data types.
+POP: Can be simpler for smaller projects or specific tasks where reusability is less important.
+Remember, both approaches have their strengths and weaknesses, and the best choice depends on your specific project requirements and goals.
+
+I hope this clarifies the key differences between OOP and POP! Feel free to ask if you have further questions about specific concepts or want to compare them in more detail.
+
+*/

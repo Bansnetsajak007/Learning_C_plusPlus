23bd22f86adc94028ecdd5d80489d8a3c1d4f7a3
sajak <sajakbasnet77@gmail.com>
2023-12-30 19:06:24 +0545

    virtual base class program added

---
+++ AllProblems/virtualBaseClass.cpp
@@ -0,0 +1,34 @@
+/*
+A virtual base class in C++ is a base class that is declared as virtual in a derived class to ensure a single shared instance of the base class in the hierarchy, addressing the "diamond problem" in multiple inheritance.
+*/
+
+#include <iostream>
+
+// Virtual base class
+class Animal {
+public:
+    virtual void makeSound() const {
+        std::cout << "Animal sound" << std::endl;
+    }
+};
+
+// Base class 1
+class Mammal : public virtual Animal {
+};
+
+// Base class 2
+class Bird : public virtual Animal {
+};
+
+// Derived class inheriting from both Mammal and Bird
+class Platypus : public Mammal, public Bird {
+};
+
+int main() {
+    Platypus p;
+
+    // Calls the overridden function in Animal
+    p.makeSound();
+
+    return 0;
+}

ca0c032f00188f4ef2fb6910ab0eeba7611c6f52
sajak <sajakbasnet77@gmail.com>
2024-01-02 08:33:36 +0545

    modified

--- oldQuestionsSolutions/overloadFriend.cpp
+++ oldQuestionsSolutions/overloadFriend.cpp
@@ -1,3 +1,43 @@
+
+/*
+without using fried function(much simpler approach)
+#include<iostream>
+
+using namespace std;
+
+class Complex {
+private:
+    double real;
+    double img;
+
+public:
+    Complex(double real, double img) : real(real), img(img) {}
+
+    Complex operator+(const Complex& other) const {
+        return Complex(real + other.real, img + other.img);
+    }
+
+    void display() {
+        cout << real << " + " << img << "i" << std::endl;
+    }
+};
+
+int main() {
+    Complex c1(6.7, 2.2);
+    Complex c2(2.33, 4.008);
+
+    Complex result = c1 + c2;
+
+    result.display();
+
+    return 0;
+}
+
+*/
+
+
+
+
 #include<iostream>
 
 using namespace std;
